f2753c9b58f8318431458f979de14cca020946b1
Carlos Velasco <xtreemze@gmail.com>
2026-09-11 10:14:14 +0800

    Deploy the custom configurator to GitHub Pages
    * Configure Vite base path for static hosting
    
    * Scope routes to the configured deployment base
    
    * Make public assets deployment-base aware
    
    * Make PWA icons relative to the deployment path
    
    * Add GitHub Pages deployment workflow
    
    * Document custom fork and GitHub Pages deployment
    
    * Let Vite rewrite public asset paths from the configured base

--- src/containers/Root.tsx
+++ src/containers/Root.tsx
@@ -1,10 +1,18 @@
 import {Provider} from 'react-redux';
+import {Router} from 'wouter';
 
 import {store} from '../store';
 import Routes from '../Routes';
 
+const routerBase =
+  import.meta.env.BASE_URL === '/'
+    ? undefined
+    : import.meta.env.BASE_URL.replace(/\/$/, '');
+
 export default () => (
   <Provider store={store}>
-    <Routes />
+    <Router base={routerBase}>
+      <Routes />
+    </Router>
   </Provider>
 );
