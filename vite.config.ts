d07eeb75ce79023b84b985d25fdf9a1befd753ca
Carlos Velasco <xtreemze@gmail.com>
2026-09-11 10:07:10 +0800

    Configure Vite base path for static hosting

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ const hash = fs.readFileSync('public/definitions/hash.json', 'utf8');
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.VITE_BASE_PATH || '/',
   plugins: [
     react(),
     createHtmlPlugin({
