05a732bdce270ffe7e195cd8ce1a3ee399fd0966
Carlos Velasco <xtreemze@gmail.com>
2026-09-11 10:25:20 +0800

    Show RGB Profiles when firmware exposes the capability

--- src/components/panes/configure-panes/lighting.tsx
+++ src/components/panes/configure-panes/lighting.tsx
@@ -1,4 +1,4 @@
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
 import styled from 'styled-components';
 import {OverflowCell, SubmenuCell, SubmenuRow} from '../grid';
 import {CenterPane} from '../pane';
@@ -12,9 +12,12 @@ import {
   AdvancedLightingValues,
   AdvancedPane,
 } from './submenus/lighting/advanced';
+import {ProfilesPane} from './submenus/lighting/profiles';
 import {getLightingDefinition, isVIADefinitionV2} from '@the-via/reader';
 import {useAppSelector} from 'src/store/hooks';
 import {getSelectedDefinition} from 'src/store/definitionsSlice';
+import {getSelectedKeyboardAPI} from 'src/store/devicesSlice';
+import {probeRGBProfileCapabilities} from 'src/utils/rgb-profile-api';
 import type {FC} from 'react';
 import {useTranslation} from 'react-i18next';
 
@@ -22,6 +25,7 @@ export const Category = {
   General: {label: 'General', Menu: GeneralPane},
   Layout: {label: 'Layout', Menu: LayoutPane},
   Advanced: {label: 'Advanced', Menu: AdvancedPane},
+  Profiles: {label: 'RGB Profiles', Menu: ProfilesPane},
 };
 
 const LightingPane = styled(CenterPane)`
@@ -43,8 +47,35 @@ const MenuContainer = styled.div`
 export const Pane: FC = () => {
   const {t} = useTranslation();
   const selectedDefinition = useAppSelector(getSelectedDefinition);
+  const keyboardApi = useAppSelector(getSelectedKeyboardAPI);
 
   const [selectedCategory, setSelectedCategory] = useState(Category.General);
+  const [hasRGBProfiles, setHasRGBProfiles] = useState(false);
+
+  useEffect(() => {
+    let cancelled = false;
+    setHasRGBProfiles(false);
+
+    if (!keyboardApi) {
+      return;
+    }
+
+    void probeRGBProfileCapabilities(keyboardApi).then((capabilities) => {
+      if (!cancelled) {
+        setHasRGBProfiles(capabilities !== null);
+      }
+    });
+
+    return () => {
+      cancelled = true;
+    };
+  }, [keyboardApi]);
+
+  useEffect(() => {
+    if (!hasRGBProfiles && selectedCategory === Category.Profiles) {
+      setSelectedCategory(Category.General);
+    }
+  }, [hasRGBProfiles, selectedCategory]);
 
   const getMenus = () => {
     if (!isVIADefinitionV2(selectedDefinition)) {
@@ -70,6 +101,7 @@ export const Pane: FC = () => {
       Category.General,
       ...(hasLayouts ? [Category.Layout] : []),
       ...(hasAdvanced ? [Category.Advanced] : []),
+      ...(hasRGBProfiles ? [Category.Profiles] : []),
     ].filter(({Menu}) => !!Menu);
   };
 
